7bc106544470dc1f62a8649aaa0e3dc661e2a29f
damayamdev <164294520+damayamdev@users.noreply.github.com>
2024-04-26 23:51:50 -0500

    Ajustes en el enpoint updateavailability

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -15,7 +15,7 @@ export const createProduct = async (req: Request, res: Response) => {
 export const getProducts = async (req: Request, res: Response) =>{
     const products = await Product.findAll({
         order:[
-            ['price','ASC']
+            ['id','ASC']
         ],
         attributes: {
             exclude:['createdAt', 'updatedAt']
@@ -70,14 +70,13 @@ export const updateProduct = async (req: Request, res: Response) =>{
 export const updateAvailability = async (req: Request, res: Response) =>{
     const {id} = req.params
     const product = await Product.findByPk(id)
-
     if (!product) {
         return res.status(404).json({
             error: 'Producto no encontrado '
         })
     }
     product.availability = !product.dataValues.availability
-    await product.save()
+    await product.update(product)
     res.json({
         status: 201,
         data: product
